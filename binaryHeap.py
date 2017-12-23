42da547913a5d3701bcef0ac061e34f9c8588083
udwivedi394 <utkarshdwivedi394@gmail.com>
2017-12-24 03:49:27 +0530

    Deleting binaryHeapUtility.py and restoring original version of binaryHeap.py

--- binaryHeap.py
+++ binaryHeap.py
@@ -1,7 +1,9 @@
 #This function returns only index if value=0
 #If value=1 and arr is given then returns value at the location
-import binaryHeapUtility as bHeap
+import sys
+import math
 
+INF = 798465416546
 def parent(i, arr=None, value=0):
 	if i == 0:
 		i = 1
@@ -27,6 +29,120 @@ def swap(x,y,arr):
 	arr[x] += arr[y]
 	arr[y] = arr[x]-arr[y]
 	arr[x] = arr[x]-arr[y]
+
+def printHeap(arr):
+	n = len(arr)
+	cur_level = 0
+
+	#print number while tree is exhausted
+	while n > 0:
+		i = 0
+		#2**cur_level -> no of elements at current level
+		while i < (2**cur_level) and n > 0:
+			#-1 is added to compensate the 2*cur_level value which starts with 1
+			print arr[2**cur_level+i-1],
+			n -= 1
+			i += 1
+		print
+		cur_level += 1
+
+#Print Reverse Tree
+#This function prints the reverse tree with value starting from 1 upto the n level
+def printTreeReverse(n):
+	cur_level = 0
+	num = 1
+
+	while cur_level < n:
+		#Flag to print the relationship between children
+		alter = False
+
+		#Print the initial space
+		sys.stdout.write("  "*(2**(cur_level)-1))
+		for i in range(2**(n-cur_level-1)):
+			#Print the number
+			sys.stdout.write("%02d"%(num))
+			num += 1
+			
+			#If the number is last of the current level, dont print between spaces
+			if i == 2**(n-cur_level-1)-1:
+				break
+
+			if alter:
+				#Print the space between two different childs
+				sys.stdout.write("  "*(2**(cur_level+1)-1))
+			else:
+				#Print the start between two same children of the parent
+				sys.stdout.write("**"*(2**(cur_level+1)-1))
+
+			#Toggle the flag value
+			alter ^= True
+		cur_level += 1
+		print
+
+#This function prints the tree with values starting from 1 upto the n level
+def printTree(n):
+	cur_level = 0
+	num = 1
+
+	while cur_level < n:
+		#Flag to print the relationship between children
+		alter = False
+
+		#Print the initial space
+		sys.stdout.write("  "*(2**(n-cur_level-1)-1))
+		for i in range(2**cur_level):
+			#Print the number
+			sys.stdout.write("%02d"%(num))
+			num += 1
+			
+			#If the number is last of the current level, dont print between spaces
+			if i == 2**(cur_level)-1:
+				break
+
+			if alter:
+				#Print the space between two different childs
+				sys.stdout.write("  "*(2**(n-cur_level)-1))
+			else:
+				#Print the start between two same children of the parent
+				sys.stdout.write("**"*(2**(n-cur_level)-1))
+
+			#Toggle the flag value
+			alter ^= True
+		cur_level += 1
+		print
+
+#This function prints the Heap in tree format
+def printGivenHeap(arr):
+	cur_level = 0
+	n = int(math.ceil(math.log(len(arr)+1,2)))
+	num = 0
+
+	while cur_level < n:
+		#Flag to print the relationship between children
+		alter = False
+
+		#Print the initial space
+		sys.stdout.write("  "*(2**(n-cur_level-1)-1))
+		for i in range(2**cur_level):
+			#Print the number
+			sys.stdout.write("%02d"%(arr[num]))
+			num += 1
+			
+			#If the number is last of the current level, dont print between spaces
+			if i == 2**(cur_level)-1 or num >= len(arr):
+				break
+
+			if alter:
+				#Print the space between two different childs
+				sys.stdout.write("  "*(2**(n-cur_level)-1))
+			else:
+				#Print the start between two same children of the parent
+				sys.stdout.write("**"*(2**(n-cur_level)-1))
+
+			#Toggle the flag value
+			alter ^= True
+		cur_level += 1
+		print
 
 def insert(arr, val):
 	#insert the value at leaf node
@@ -60,7 +176,7 @@ arr = []
 for i in range(1,10):
 	insert(arr,i)
 print "Before:"
-bHeap.printGivenHeap(arr)
+printGivenHeap(arr)
 delete(arr,0)
 print "After:"
-bHeap.printGivenHeap(arr)
+printGivenHeap(arr)
